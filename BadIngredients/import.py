2b3bab70ca24f08456fc4e87914b837ada219ed3
sarahgronemann <sarah.gronemann01@stud.uni-goettingen.de>
2023-02-08 15:40:50 +0100

    venv umbenannt

--- BadIngredients/import.py
+++ BadIngredients/import.py
@@ -1,15 +1,17 @@
 # first created venv for PythonProject
-# python(3) -m venv project_env
-# source project_env/bin/activate
+# python(3) -m venv venv
+# source venv/bin/activate
 
 import pyodbc as odbc # pip(3) install pyodbc;  use libary to connect to MS SQL Server
 import pandas as pd # pip(3) install pandas; clean up the data
+
+# pip(3) list ; show the installed packages
 
 # Step 1. Importing dataset from CSV
 
 df = pd.read_csv('badingredients.csv')
 
-# Step 2. Data cleanup
+# Step 2. Data cleanuppip
 
 
 
