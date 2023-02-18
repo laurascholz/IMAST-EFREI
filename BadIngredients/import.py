71e178b818fa162116c22bb602d5e713596d09dd
sarahgronemann <sarah.gronemann01@stud.uni-goettingen.de>
2023-02-18 16:49:43 +0100

    csv import update

--- BadIngredients/import.py
+++ BadIngredients/import.py
@@ -2,20 +2,23 @@
 # python(3) -m venv venv
 # source venv/bin/activate
 
-import pyodbc as odbc # pip(3) install pyodbc;  use libary to connect to MS SQL Server
-import pandas as pd # pip(3) install pandas; clean up the data
+import pyodbc  # pip(3) install pypyodbc;  use libary to connect to MS SQL Server
+import pandas  # pip(3) install pandas; clean up the data
 
 # pip(3) list ; show the installed packages
 
 # Step 1. Importing dataset from CSV
 
-df = pd.read_csv('badingredients.csv')
+#df = pd.read_csv('badingredients.csv')
 
-# Step 2. Data cleanuppip
+# Step 2. Data cleanup
 
-
+# df['']
 
 # database connection
 DRIVER = 'SQL Server' # or mysqlserverefrei or MSQL Server
 SERVER_NAME = 'mysqlserverefrei.database.windows.net'
 DATABASE_NAME = 'efrei'
+Uid = 'azureuser'
+Pwd = 'Efrei2023'
+Trusted_Connection='yes'
---
+++ BadIngredients/pyodbc.py
@@ -0,0 +1,9 @@
+import pyodbc
+
+
+DRIVER = 'SQL Server' # or mysqlserverefrei or MSQL Server
+SERVER_NAME = 'mysqlserverefrei.database.windows.net'
+DATABASE_NAME = 'efrei'
+Uid = 'azureuser'
+Pwd = 'Efrei2023'
+Trusted_Connection='yes'
