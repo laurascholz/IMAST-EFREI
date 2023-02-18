393b0c6516f6fa659c9a7063d5381e463726d313
sarahgronemann <sarah.gronemann01@stud.uni-goettingen.de>
2023-02-18 17:08:41 +0100

    csv update

--- BadIngredients/import.py
+++ BadIngredients/import.py
@@ -2,15 +2,15 @@
 # python(3) -m venv venv
 # source venv/bin/activate
 
-import pyodbc  # pip(3) install pypyodbc;  use libary to connect to MS SQL Server
-import pandas  # pip(3) install pandas; clean up the data
+import pyodbc as odbc # pip(3) install pypyodbc;  use libary to connect to MS SQL Server
+import pandas as pd # pip(3) install pandas; clean up the data
 
 # pip(3) list ; show the installed packages
 
 # Step 1. Importing dataset from CSV
 
-#df = pd.read_csv('badingredients.csv')
-
+df = pd.read_csv('BadIngredients/badingredients.csv')
+print(df)
 # Step 2. Data cleanup
 
 # df['']
