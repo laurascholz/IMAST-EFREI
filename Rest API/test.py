c32df87537b7a7dab0f369911425900cdeb49b5a
sarahgronemann <sarah.gronemann01@stud.uni-goettingen.de>
2023-02-22 20:09:56 +0100

    Update test.py

--- Rest API/test.py
+++ Rest API/test.py
@@ -2,5 +2,19 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
+data = [{"likes": 78, "name": "Joe", "views":1000000},
+        {"likes": 10, "name": "How to make REST API", "views":800000},
+        {"likes": 35, "name": "Tim", "views":2000}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data [i])
+    print(response.json())
+
+input ()
+
+response = requests.delete(BASE + "video/0")
+print(response)
+
+input()
+response = requests.get(BASE + "video/6")
 print(response.json())
