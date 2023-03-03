47c2b8d886ddd52ffd627eec4ae0af3d4fd84303
sarahgronemann <sarah.gronemann01@stud.uni-goettingen.de>
2023-03-03 15:04:47 +0100

    added insert_search_string

--- Product_Insert/insert.py
+++ Product_Insert/insert.py
@@ -6,6 +6,65 @@ import pypyodbc as odbc # pip(3) install pypyodbc;  use libary to connect to MS 
 
 # pip(3) list ; show the installed packages
 
+def searchstring_insert(search_string, search_link):
+
+    """
+    Step 1.1 Create SQL Server Connection String
+    """
+
+    DRIVER = 'ODBC Driver 18 for SQL Server' 
+    SERVER_NAME = 'mysqlserverefrei.database.windows.net'
+    DATABASE_NAME = 'efrei'
+
+    def connection_string(driver, server_name, database_name):
+        conn_string = f"""
+            DRIVER={{{driver}}};
+            SERVER={server_name};
+            DATABASE={database_name};
+            Trust_Connection=yes; 
+            Uid=azureuser;
+            PwD=Efrei2023;         
+        
+        """
+        return conn_string
+    #print(connection_string(DRIVER,SERVER_NAME,DATABASE_NAME)) # to check the definition  
+
+    """"
+    #Step 1.2 Create database connection instance
+    """
+    try:
+        conn = odbc.connect(connection_string(DRIVER, SERVER_NAME, DATABASE_NAME))
+    except odbc.DatabaseError as e:
+        print('Database Error:')    
+        print(str(e.value[1]))
+    except odbc.Error as e:
+        print('Connection Error:')
+        print(str(e.value[1]))
+    # print (conn) #- show connnection
+
+    
+    """
+    #Step 1.3 Create a cursor connection and insert search string
+    """
+    #search_date will insert automatically
+    sql_insert = '''
+         INSERT INTO searchstring(search_string,search_link,website_name) 
+             VALUES (?,?,?)
+     '''
+       
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql_insert,(search_string, search_link, 'Sephora')) #currently we only search on Sephora
+        cursor.commit();    
+    except Exception as e:
+        cursor.rollback()
+        print(str(e))
+    finally:
+        print('Search String saved in Database.')
+        cursor.close()
+        conn.close() 
+
+
 def product_insert(api_id, product_name, product_brand, product_price, product_link, product_image):
 
     """
@@ -44,7 +103,7 @@ def product_insert(api_id, product_name, product_brand, product_price, product_l
 
     
     """
-    #Step 1.3 Create a cursor connection and insert records if product not exists in database
+    #Step 1.3 Create a cursor connection and insert product
     """
     
     sql_insert = '''
@@ -107,7 +166,7 @@ def search_product(api_id, product_name):
     
     sql_select = '''
          SELECT product_name,product_brand,product_price,product_link,product_image FROM product
-            WHERE api_id = ? AND product_name = ?;
+            WHERE api_id = ? AND product_name = ?
      '''
        
     try:
--- Product_Insert/test.py
+++ Product_Insert/test.py
@@ -2,8 +2,8 @@ import insert
 
 try:
        #insert.product_insert(126, 'Mascara', 'Lancome', '24.00 €', 'www.sephora.de/lancome/mascara', 'www.sehora.de/image/mascara')
-       insert.search_product(126, 'Mascara')
-       
+       #insert.search_product(126, 'Mascara')
+       insert.searchstring_insert('Mascara','www.sephora.de')
 except:
        print("Fehler bei Suche.")
     
