6bf1edaa750e7bf2cb082367a6f452ef35c07dc3
laurascholz <64077869+laurascholz@users.noreply.github.com>
2023-03-03 13:27:40 +0100

    about page text 1. version

--- IMAST-EFREI/src/routes/about/+page.svelte
+++ IMAST-EFREI/src/routes/about/+page.svelte
@@ -1,3 +1,17 @@
-<h1>about</h1>
-<p>this page is the about page.</p>
+<h1>About us</h1>
+
 <p>On this page, you can find information about our vision, our business plan and the source we use to identify bad ingredients.</p>
+
+
+<h3>Our Vision</h3>
+<article> 
+    Our vision is to enable everyone to use only the <b> best </b> product. 
+    To be able to understand the ingredients is the first step for that.
+    Our website offers a free solution to check the ingredients of the products you are interested in. 
+    You can see all the harmful ingredients and decide, if the product is healthy enough for <b> your </b> life.
+</article>
+
+<h3>Our Sources</h3>
+<article>
+    The Cosmectic Checker uses a database....
+</article>
