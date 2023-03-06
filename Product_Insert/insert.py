e16a44462440d5047a41f2af3494c976b4ab3bf5
sarahgronemann <sarah.gronemann01@stud.uni-goettingen.de>
2023-03-06 14:11:02 +0100

    product_select and product_update

--- Product_Insert/insert.py
+++ Product_Insert/insert.py
@@ -200,6 +200,12 @@ def searchstring_update(search_string):
         cursor.close()
         conn.close()
 
+
+"""
+    Methods for product
+"""
+
+
 def product_insert(search_string, api_id, product_name, product_brand, product_price, product_link, product_image): #method to save product in database with relation to search string
 
     """
@@ -291,7 +297,7 @@ def product_insert(search_string, api_id, product_name, product_brand, product_p
         
         cursor.execute(sql_insert_productsearch,(product_id,searchstring_id))
         cursor.commit(); 
-        # productsearch inserted
+        # productsearch (Relation Product_Searchstring) inserted
         
     except Exception as e:
         cursor.rollback()
@@ -351,7 +357,7 @@ def product_select(api_id, product_name):
         cursor = conn.cursor()
         cursor.execute(sql_select,(api_id, product_name))
         data_list = cursor.fetchall()
-        if data_list == []:
+        if data_list == []:     # check if product exists in database
             print('404: Not found in Database')
             return 404
             
@@ -369,6 +375,122 @@ def product_select(api_id, product_name):
         print('Product search completed.')
         cursor.close()
         conn.close() 
+        
+
+def product_update(search_string, api_id, product_name, product_price, product_link, product_image): #method to save update product and add a relation to search string if doesnt exist
+
+    """
+    Step 1.1 Create SQL Server Connection String
+    """
+
+    DRIVER = 'ODBC Driver 18 for SQL Server' 
+    SERVER_NAME = 'mysqlserverefrei.database.windows.net'
+    DATABASE_NAME = 'efrei'
+
+    def connection_string(driver, server_name, database_name):
+        conn_string = f"""
+            DRIVER={{{driver}}};
+            SERVER={server_name};
+            DATABASE={database_name};
+            Trust_Connection=yes; 
+            Uid=azureuser;
+            PwD=Efrei2023;         
+        
+        """
+        return conn_string
+    #print(connection_string(DRIVER,SERVER_NAME,DATABASE_NAME)) # to check the definition  
+
+    """"
+    #Step 1.2 Create database connection instance
+    """
+    try:
+        conn = odbc.connect(connection_string(DRIVER, SERVER_NAME, DATABASE_NAME))
+    except odbc.DatabaseError as e:
+        print('Database Error:')    
+        print(str(e.value[1]))
+    except odbc.Error as e:
+        print('Connection Error:')
+        print(str(e.value[1]))
+    # print (conn) #- show connnection
+
+    
+    """
+    #Step 1.3 Create a cursor connection and insert product as well as insert product_id and searchstring_id in tabel productsearch
+    """
+    
+    sql_update_product = '''
+         UPDATE product
+            SET product_price = ?, product_link = ?, product_image = ?
+             WHERE api_id = ? AND product_name = ? 
+     '''
+    
+    sql_select_product_id = '''
+         SELECT id FROM product
+            WHERE api_id = ? AND product_name = ?
+     '''
+    
+    sql_select_searchstring_id = '''
+         SELECT id FROM searchstring
+            WHERE search_string = ? AND website_name = ?
+     '''       
+    
+    sql_select_productsearch = '''
+         SELECT * FROM productsearch
+            WHERE product_id = ? AND searchstring_id = ?
+     '''  
+     
+    sql_insert_productsearch = '''
+         INSERT INTO productsearch(product_id,searchstring_id) 
+             VALUES (?,?)
+     '''
+       
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql_update_product,(product_price,product_link,product_image,api_id,product_name))
+        cursor.commit()
+        print('Product updatded')
+        # product updated with price, link and image 
+        
+        cursor.execute(sql_select_product_id,(api_id,product_name))
+        product = cursor.fetchone()
+        if product == []:   
+            print('404: Product not found in Database')
+            return 404
+        else:  
+            product_id = product[0]
+            print(product_id)
+        #cursor.commit()      
+        # product_id searched
+        
+        cursor.execute(sql_select_searchstring_id,(search_string,'Sephora'))
+        searchstring = cursor.fetchone()
+        if searchstring == []:
+            print('404: Search String not found in Database')
+            return 404
+        else: 
+            searchstring_id = searchstring[0]
+            print(searchstring_id)
+        #cursor.commit(); 
+        # searchstring_id searched   
+        
+        cursor.execute(sql_select_productsearch,(product_id,searchstring_id))   
+        productsearch = cursor.fetchone()
+        # print(productsearch)
+        if productsearch == None:   #check if relation doesnt exists
+            cursor.execute(sql_insert_productsearch,(product_id,searchstring_id)) #insert new relation
+            cursor.commit(); 
+            print('Relation Product-Searchstring added')
+            # productsearch inserted
+        else:
+            print("Relation Searchstring to Product already exists")
+    except Exception as e:
+        cursor.rollback()
+        print(str(e))
+    finally:
+        print('Product and Relation to searchstring updated.')
+        cursor.close()
+        conn.close()        
+        
 
 def ingredients_insert(api_id, product_name, ingredients_list): #method to save ingredients in database with relation to product
 
@@ -405,7 +527,6 @@ def ingredients_insert(api_id, product_name, ingredients_list): #method to save 
         print('Connection Error:')
         print(str(e.value[1]))
     # print (conn) #- show connnection
-
     
     """
     #Step 1.3 Create a cursor connection and insert ingredients as well as insert ingredient_id in tabel product
--- Product_Insert/test.py
+++ Product_Insert/test.py
@@ -1,17 +1,26 @@
 import insert
 
 try:
-       searchstring = 'Boss'
-       #insert.searchstring_insert('Blush')
-       if insert.searchstring_select(searchstring) == 404:   #if search string doesnt exist in database
-              insert.searchstring_insert(searchstring)       # insert in database
-       else: 
-              insert.searchstring_update(searchstring)       # else update search_date    
+       searchstring = 'red'
+       api_id = '123'
+       product_name = 'Sì Passione - Coffret Eau de Parfum'
+       product_brand = 'ARMANI'
+       product_price = '123'
+       product_link = 'https://www.sephora.fr/p/s%C3%AC-passione.de'
+       product_image = 'https://www.sephora.fr/dw/image/v2/BCVW_PRD/on/demandware.static/123'
        
-       #insert.searchstring_update('Mascara')
+       #if insert.searchstring_select(searchstring) == 404:   #if search string doesnt exist in database
+       #      insert.searchstring_insert(searchstring)       # insert in database
+       #else: 
+         #     insert.searchstring_update(searchstring)       # else update search_date    
+       
+       if insert.product_select(api_id, product_name) == 404:   #if product doesnt exist in database
+              insert.product_insert(searchstring,api_id,product_name,product_brand,product_price,product_link,product_image) # insert in database
+       else: 
+              insert.product_update(searchstring, api_id, product_name, product_price, product_link,product_image)  # else update product_price, product_link and insert a new relation if neccessary
+       
        #insert.product_insert('Chanel',139, 'Mascara', 'Chanel', '24.00 €', 'www.sephora.de/chanel/mascara', 'www.sehora.de/image/mascara')
        #insert.search_product(126, 'Mascara')
-       #insert.searchstring_insert('Lancome')
 
        #insert.ingredients_insert(517720, 'Lip + Cheek - Blush Crème', 'WATER/AQUA/EAU, COPERNICIA CERIFERA (CARNAUBA) WAX/CERA CARNAUBA/CIRE DE CARNAUBA, GLYCERYL STEARATE')
        
