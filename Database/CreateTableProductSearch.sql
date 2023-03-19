729ea2b49f41d37778f60491f6f52975f81bc466
sarahgronemann <sarah.gronemann01@stud.uni-goettingen.de>
2023-03-19 16:03:38 +0100

    explantion method

--- Database/CreateTableProductSearch.sql
+++ Database/CreateTableProductSearch.sql
@@ -9,6 +9,7 @@ CREATE TABLE productsearch
 (
     product_id INT NOT NULL,
     searchstring_id INT NOT NULL,
+    product_name VARCHAR(1000) NOT NULL, 
     CONSTRAINT FK_productsearch_product FOREIGN KEY (product_id) REFERENCES product(id),
     CONSTRAINT FK_productsearch_searchstring FOREIGN KEY (searchstring_id) REFERENCES searchstring(id)
 );
